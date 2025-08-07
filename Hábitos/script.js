867b031c8051f81bfb8674eef7abfe88bfcb35ee
Jok <82481025+Joakin-2@users.noreply.github.com>
2025-08-07 15:50:44 -0300

    Update script.js

--- Hábitos/script.js
+++ Hábitos/script.js
@@ -765,6 +765,7 @@ function abrirSitesManha() {
         // Sábado
         window.open('https://www.twitch.tv/joakinbr', '_blank');
         window.open('https://business.facebook.com/latest/content_calendar?business_id=2349354548756195&asset_id=617136918158059', '_blank');
+        window.open('https://pt.duolingo.com/learn', '_blank');
         window.open('https://j2plus.netlify.app/gym/', '_blank');
         window.open('https://j2plus.netlify.app/sax/', '_blank');
         window.open('https://j2plus.netlify.app/proto/christ/crist.html', '_blank');
@@ -772,12 +773,14 @@ function abrirSitesManha() {
         // Domingo
         window.open('https://www.twitch.tv/joakinbr', '_blank');
         window.open('https://business.facebook.com/latest/content_calendar?business_id=2349354548756195&asset_id=617136918158059', '_blank');
+        window.open('https://pt.duolingo.com/learn', '_blank');
         window.open('https://j2plus.netlify.app/sax/', '_blank');
         window.open('https://j2plus.netlify.app/proto/christ/crist.html', '_blank');
     } else {
         // Dias úteis (Segunda a Sexta)
         window.open('https://www.twitch.tv/joakinbr', '_blank');
         window.open('https://business.facebook.com/latest/content_calendar?business_id=2349354548756195&asset_id=617136918158059', '_blank');
+        window.open('https://pt.duolingo.com/learn', '_blank');
         window.open('https://j2plus.netlify.app/gym/', '_blank');
         window.open('https://j2plus.netlify.app/sax/', '_blank');
         window.open('https://j2plus.netlify.app/proto/christ/crist.html', '_blank');
